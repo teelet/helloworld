1700d2e077f83833114bb59abc039aa87cba35a9
shaohua <shaohua@bj-m-14760a.local>
2016-06-22 10:43:20 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"fmt"
+	"strconv"
+)
+
+type item struct {
+	id int
+	name string
+}
+
+func main()  {
+	fmt.Println("helloworld")
+	db, err := sql.Open("mysql", "test:123456@tcp(139.129.36.196:3306)/test?charset=utf8")
+	defer db.Close()
+	checkErr(err)
+
+	err = db.Ping()
+	checkErr(err)
+	stmt, err := db.Prepare("select * from test limit 10")
+	checkErr(err)
+	rows, err := stmt.Query()
+	checkErr(err)
+	for rows.Next() {
+		i := new(item)
+		rows.Scan(&(i.id), &(i.name))
+		fmt.Println(strconv.Itoa(i.id) + " : " + i.name)
+	}
+}
+
+func checkErr(err error){
+	if err != nil {
+		log.Panic(err.Error())
+	}
+}
